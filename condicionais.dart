0614d2dff28f3a2aeb487c1fb27b967c80dc11c6
MichelecFernandes <michelecristinafr@gmail.com>
2025-04-15 20:11:13 -0300

    feat: maneira diferente de criar um construtor

---
+++ condicionais.dart
@@ -0,0 +1,12 @@
+void main(){
+  int idade = 17;
+
+  if(idade >17){
+    print("Maior de idade");
+  }else{
+    print("Menor de idade");
+  }
+
+  print(idade >= 18 ? "Maior de idade" : "Menor de idade");
+
+}
