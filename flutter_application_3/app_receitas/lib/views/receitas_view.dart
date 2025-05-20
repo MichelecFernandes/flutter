3a970e18cf8a748aa103eb4ed71fd98a9a6ee28b
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-20 19:55:32 -0300

    feat: criacao do diretorio das telas iniciais do projeto

---
+++ flutter_application_3/app_receitas/lib/views/receitas_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReceitasView extends StatefulWidget {
+  const ReceitasView({super.key});
+
+  @override
+  State<ReceitasView> createState() => _ReceitasViewState();
+}
+
+class _ReceitasViewState extends State<ReceitasView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
