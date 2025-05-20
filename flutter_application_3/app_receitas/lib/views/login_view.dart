06d1d6eb4f692ba8f230e9bcb392ee0f4bee81a9
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-20 20:27:25 -0300

    feat: adicao de header na tela

--- flutter_application_3/app_receitas/lib/views/login_view.dart
+++ flutter_application_3/app_receitas/lib/views/login_view.dart
@@ -10,11 +10,36 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-      
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 26,
+            vertical: 16
+          ),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    'Acessar sua conta',              style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600
+                  ),
+                  ),
+                  TextFormField(
+
+                  )
+                ],
+              ),
+          ),
+        ),
+      ),
     );
   }
 }
