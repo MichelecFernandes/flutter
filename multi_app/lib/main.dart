f9f46066ddc4e1a16bd8e20c9a5204c3c064fb3c
MichelecFernandes <michelecristinafr@gmail.com>
2025-08-05 20:04:30 -0300

    feat: atribuindo o fundo padrao do sistema no app

--- multi_app/lib/main.dart
+++ multi_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:multi_app/shared/app_constants.dart';
 import 'package:multi_app/shared/app_theme.dart';
+import 'package:multi_app/views/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: AppConstants.appName,
       theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage()
+      },
     );
   }
 }
--- multi_app/lib/shared/app_theme.dart
+++ multi_app/lib/shared/app_theme.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
-    return ThemeData();
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light, 
+      scaffoldBackgroundColor: Colors.white
+    );
   }
 
     static ThemeData get darkTheme {
-    return ThemeData();
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 5, 69, 88)
+    );
   }
 }
