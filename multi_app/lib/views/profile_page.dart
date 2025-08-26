fee2aacb363e7af0f2bafd8044c795b20815ad9a
MichelecFernandes <michelecristinafr@gmail.com>
2025-08-26 20:53:08 -0300

    feat: apresentando informacoes do usuario na pagina de perfil

--- multi_app/lib/views/profile_page.dart
+++ multi_app/lib/views/profile_page.dart
@@ -12,22 +12,79 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(context: context, title: AppConstants.appName),
-      body: ResponsiveContainer(child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 60,
-            child: user.image != null ? Image.network(user.image!) : Icon(Icons.person, size: 60)
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            '${user.firstName} ${user.lastName ?? ''}',
-            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-              fontWeight: FontWeight.bold
+      body: ResponsiveContainer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              child: user.image != null
+                  ? Image.network(user.image!)
+                  : Icon(Icons.person, size: 60),
             ),
-          )
-        ],
-      )),
+            SizedBox(height: 16.0),
+            Text(
+              '${user.firstName} ${user.lastName ?? ''}',
+              style: Theme.of(
+                context,
+              ).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(user.email),
+            SizedBox(height: 24.0),
+            Card(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    // Linhas com dados do usuario
+                    _buildProfileRow(
+                      icon: Icons.badge, 
+                      label: 'ID', 
+                      text:user.id.toString(),
+                      theme: Theme.of(context),
+                      ),
+                      Divider(),
+                      _buildProfileRow(
+                      icon: Icons.person, 
+                      label: 'Nome', 
+                      text: '${user.firstName} ${user.lastName ?? ''}',
+                      theme: Theme.of(context))
+                      ,
+                      Divider(),
+                      _buildProfileRow(
+                      icon: Icons.email, 
+                      label: 'E-mail', 
+                      text: user.email,
+                      theme: Theme.of(context))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileRow({ 
+    required IconData icon,
+    required String label,
+    required String text,
+    required ThemeData theme,
+  }) {
+    return Row(
+      children: [
+        Icon(icon, color: theme.colorScheme.primary),
+        SizedBox(width: 8.0),
+        Expanded(child: Text(label, style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold))),
+        Expanded(child: Text(text, style: theme.textTheme.bodyLarge, textAlign: TextAlign.end, softWrap: false, overflow: TextOverflow.ellipsis,)),
+
+      ],
     );
   }
 }
